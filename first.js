e2eed5e60a03b4890818d59ab654eed0d17ca2ca
Mohammed Usman <imailusman07@gmail.com>
2025-05-29 18:27:36 +0530

    Variables in JS

--- first.js
+++ first.js
@@ -1,2 +1,13 @@
 // 1st JS Code
 console.log("Hello, Mohammed Usman!");
+// Variables in JS
+fullName="Mohammed Usman";
+age=21;
+x=null;
+y=undefined;
+isFollow=true;
+console.log(fullName);
+console.log(age);
+console.log(x);
+console.log(y);
+console.log(isFollow);
