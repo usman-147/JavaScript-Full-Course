e344743f7ba4b3532e2c095b26634b864ca7558e
Mohammed Usman <imailusman07@gmail.com>
2025-06-03 13:22:47 +0530

    Add switch statement

--- first.js
+++ first.js
@@ -181,3 +181,16 @@ if(age<18){
 }else{
     console.log("middle");
 }
+// switch statement from MDN Docs
+const expr = "Papayas";
+switch (expr) {
+  case "Oranges":
+    console.log("Oranges are $0.59 a pound.");
+    break;
+  case "Mangoes":
+  case "Papayas":
+    console.log("Mangoes and papayas are $2.79 a pound.");
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
