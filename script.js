1183c48f4d4566644fb11bf467955b717407a844
Mohammed Usman <imailusman07@gmail.com>
2025-06-16 18:22:23 +0530

    Add event to div1

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ btn1.onclick = () => {
     let a=25;
     a++;
     console.log(a);
-}
+};
+
+let div1 = document.querySelector("#div1");
+
+div1.onmouseover = () => {
+    console.log("you are inside div1");
+};
