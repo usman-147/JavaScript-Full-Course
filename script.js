b3fb46fbe956a920a5df982fde0ab34ae053e592
Mohammed Usman <imailusman07@gmail.com>
2025-07-01 14:50:24 +0530

    Classes in JS

--- script.js
+++ script.js
@@ -92,3 +92,22 @@ const karanArjun = {
 };
 console.log(karanArjun);
 karanArjun.__proto__=employee;
+
+// Classes in JS
+class ToyotaCar{
+    constructor(brand,mileage){
+        console.log(`creating new object`);
+        this.brand=brand;
+        this.mileage=mileage;
+    }
+    start(){
+        console.log(`start`);
+    }
+    stop(){
+        console.log(`stop`);
+    }
+}
+let fortuner=new ToyotaCar("fortuner",10);
+console.log(fortuner);
+let lexus=new ToyotaCar("lexus",12); // if nothings pass gives undefined rather throwing an error
+console.log(lexus);
