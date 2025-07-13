a0ae1e50c184c220a7014ec22f26bb85559c44bf
Mohammed Usman <imailusman07@gmail.com>
2025-07-13 15:38:36 +0530

    Add Callback Hell in JS

--- script.js
+++ script.js
@@ -257,7 +257,28 @@ calculator(1, 2, (a, b) => {
   console.log(a + b);
 });
 
-const hello = () => {
+const hello1 = () => {
   console.log("hello");
 };
-setTimeout(hello, 3000);
+setTimeout(hello1, 3000);
+
+// Callback Hell
+function getData(dataID, getNextData) {
+  setTimeout(() => {
+    console.log(`data: ${dataID}`);
+    if (getNextData) {
+      getNextData();
+    }
+  }, 2000);
+}
+
+getData(1, () => {
+  console.log("getting data2...");
+  getData(2, () => {
+    console.log("getting data3...");
+    getData(3, () => {
+      console.log("getting data4...");
+      getData(4);
+    });
+  });
+});
